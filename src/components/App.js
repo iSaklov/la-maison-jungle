ca53286d3417d23e212cda620c25bddd1689fa90
iSaklov <ivansaklov@gmail.com>
2023-09-08 19:07:13 +0200

    feat: add Footer

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import Banner from './Banner'
 import logo from '../assets/logo.png'
 // import Cart from './Cart'
 import ShoppingList from './ShoppingList'
+import Footer from './Footer'
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       </Banner>
       {/* <Cart /> */}
       <ShoppingList />
+      <Footer />
     </div>
   )
 }
